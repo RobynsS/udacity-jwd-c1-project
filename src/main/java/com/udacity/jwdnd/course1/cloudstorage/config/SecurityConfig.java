df0b5e7fd9ddd964200174bfa8154160bdd61fc6
Steven Robyns <robynssteven@gmail.com>
2020-07-30 21:26:12 +0200

    implementation of signup & login templates

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -34,5 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin()
                 .defaultSuccessUrl("/home", true);
+
+        //http.logout();
+
     }
 }
